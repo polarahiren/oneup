45e54b13fc31c62abb16156d7d723bd28bcfb2de
polara Hiren <hiren.polara22@gmail.com>
2022-03-12 15:53:39 +0530

    hero section design setup

--- src/js/app.js
+++ src/js/app.js
@@ -5,39 +5,13 @@ try {
 // slick js
 require('slick-carousel/slick/slick');
 
-jquery(document).ready(function () {
-    
-    // jquery('.fans-features-slider').slick({
-    //     dots: false,
-    //     speed: 300,
-    //     slidesToShow: 5,
-    //     slidesToScroll: 1,
-    //     arrows: false,
-    //     autoplay: true,
-    //     responsive: [{
-    //             breakpoint: 1800,
-    //             settings: {
-    //                 slidesToShow: 4,
-    //             }
-    //         },
-    //         {
-    //             breakpoint: 1400,
-    //             settings: {
-    //                 slidesToShow: 3,
-    //             }
-    //         },
-    //         {
-    //             breakpoint: 991,
-    //             settings: {
-    //                 slidesToShow: 2
-    //             }
-    //         },
-    //         {
-    //             breakpoint: 540,
-    //             settings: {
-    //                 slidesToShow: 1
-    //             }
-    //         }
-    //     ]
-    // });
+$(document).ready(function () {
+    $('.hero-slider').slick({
+        dots: true,
+        speed: 300,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        autoplay: true
+    });
 });
